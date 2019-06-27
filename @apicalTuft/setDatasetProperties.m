171adafca8292a400ba91ec9c281dbe4b4234520
Ali Karimi <ali.karimi@brain.mpg.de>
2019-06-27 15:31:11 +0200

    Use contain for finding the right dataset properties. This allows the dataset name deviate from a specific string as long as it contains one of the options

--- @apicalTuft/setDatasetProperties.m
+++ @apicalTuft/setDatasetProperties.m
@@ -15,40 +15,46 @@ function [obj] = setDatasetProperties(obj)
 I=eye(3);
 I_yNeg=bsxfun(@times,[1,-1,1],I);
 I_ZReplacedWithY=I([1,3,2]',:);
-switch obj.dataset
-    case 'S1'
+countNrofDatasets=0;
+if contains(obj.dataset,'S1')
+    countNrofDatasets=countNrofDatasets+1;
         obj.datasetProperties=struct('dimPiaWM', 2,...
             'correction',[0 197 0;I_yNeg],... %197
             'highResBorder',[],...
             'L1BorderInPixel',8833,'bbox',[1,9344;1,6528;1,7808]);
-    case 'V2'
+elseif contains(obj.dataset,'V2')
+        countNrofDatasets=countNrofDatasets+1;
         obj.datasetProperties=struct('dimPiaWM', 2,...
             'correction',[0 202.5 0;I],... %202.5
             'highResBorder',[],...
             'L1BorderInPixel',-1708,'bbox',[1,7168;1,10240;1,5120]);
-    case 'PPC'
+elseif contains(obj.dataset,'PPC') && ~contains(obj.dataset,'PPC2')
+        countNrofDatasets=countNrofDatasets+1;
         obj.datasetProperties=struct('dimPiaWM', 2,...
             'correction',[0 156 0;I],... %156
             'highResBorder',[],...
             'L1BorderInPixel',-1333,'bbox',[1,12032;1,12032;1,4864]);
-    case 'PPC2'
+elseif contains(obj.dataset,'PPC2')
+        countNrofDatasets=countNrofDatasets+1;
         obj.datasetProperties=struct('dimPiaWM', 2,...
             'correction',[0 0 0;I],...
             'highResBorder',[],...
             'L1BorderInPixel',13000,'bbox',[1,25881;1,55373;1,13229]);
-    case 'ACC'
+elseif contains(obj.dataset,'ACC')
+        countNrofDatasets=countNrofDatasets+1;
         obj.datasetProperties=struct('dimPiaWM', 2,...
             'correction',[0 263 0;I_yNeg],... % 263
             'highResBorder',[],...
             'L1BorderInPixel',7300,'bbox',[1,9216;1,14336;1,3328]);
-    case {'LPtA','LPtAInhibitory','LPtAExcitatory'}
+elseif contains(obj.dataset,'LPtA')
+        countNrofDatasets=countNrofDatasets+1;
         obj.datasetProperties=struct('dimPiaWM', 3,...
             'correction',[0 16.9 0;I_ZReplacedWithY],... % 17, 100 nm to make sure small bits don't end up in second bin
             'highResBorder',99940,... %  (2768*30+correction*1000)
             'L1BorderInPixel',4820,'bbox',[16001,29000;16001,26242;1,2767]);
-    otherwise
+else
         disp('datasetProperties setting failed, no dataset information present!')
 end
-
+    assert(countNrofDatasets==1);
 end
 
