c03c8e7f7558cd33e22b797ae150e63d92bb5fb0
Ali Karimi <ali.karimi@brain.mpg.de>
2019-08-05 17:09:19 +0200

    Create Separate grouping for the L5A and L2MarginalNeurons(MN)

--- +config/l2vsl3vsl5Diameter.m
+++ +config/l2vsl3vsl5Diameter.m
@@ -3,7 +3,8 @@ function [prop] = l2vsl3vsl5Diameter()
 % Author: Ali Karimi <ali.karimi@brain.mpg.de>
 prop.outputDir=fullfile(util.dir.getAnnotation,'l2vsl3vsl5Diameter');
 prop.apicalType={ 'layer2ApicalDendrite_mapping',...
-    'layer3ApicalDendrite_mapping','layer5ApicalDendrite_mapping'};
+    'layer3ApicalDendrite_mapping','layer5ApicalDendrite_mapping',...
+    'layer2MNApicalDendrite_mapping','layer5AApicalDendrite_mapping'};
 prop.fixedEnding={'start','end','exit','soma','bifurcation',...
     'dead end','newEndings'}';
 prop.legacyGrouping=false;
