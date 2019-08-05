47678be0876061b737b379e2957d2a0e8c59f4e6
Ali Karimi <ali.karimi@brain.mpg.de>
2019-08-05 14:08:40 +0200

    Add the L5A colors

--- +util/+plot/getColors.m
+++ +util/+plot/getColors.m
@@ -4,6 +4,7 @@ colors.l2color=[157/255,157/255,156/255];
 colors.dlcolor=[243/255,146/255,0/255];
 colors.l3color=[109,181,70]./255;
 colors.l5color=[158,32,103]./255;
+colors.l5Acolor=[0,113,188]./255;
 colors.exccolor=[1,0,0];
 colors.inhcolor=[0,0,1];
 end
--- +util/setColors.m
+++ +util/setColors.m
@@ -8,6 +8,7 @@ assignin('base','l2color',[157/255,157/255,156/255]);
 assignin('base','dlcolor',[243/255,146/255,0/255]);
 assignin('base','l3color',[109,181,70]./255);
 assignin('base','l5color',[158,32,103]./255);
+assignin('base','l5Acolor',[0,113,188]./255);
 assignin('base','exccolor',[1,0,0]);
 assignin('base','inhcolor',[0,0,1]);
 end
