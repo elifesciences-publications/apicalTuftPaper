da142f7443f65a8a88c63e4c0722903956066e23
Ali Karimi <ali.karimi@brain.mpg.de>
2019-08-05 18:32:39 +0200

    bugfixes

--- +dendrite/+l2vsl3vsl5/combineDistalL5A.m
+++ +dendrite/+l2vsl3vsl5/combineDistalL5A.m
@@ -1,17 +1,24 @@
-function [outTbl] = combineDistalL5A(inTbl)
+function [outTbl] = combineDistalL5A(inTbl,fun)
 %COMBINEDISTALL5A combine the results from the two distal L5A annotations
+
+if ~exist('fun','var') || isempty(fun)
+    fun = @(x)sum(x,1);
+end
+% Parameters
 L5Atag='layer5AApicalDendrite_mapping';
-inTbl=sortrows(inTbl);
-outTbl=table('Size',[width(inTbl),3],'VariableTypes',...
-    repmat({'double'},width(inTbl),1),'VariableNames',...
+numL5Acells=3;
+% Generate output table
+inTbl=sortrows(inTbl,1);
+varTypes=varfun(@class,inTbl,'OutputFormat','cell');
+outTbl=table('Size',[numL5Acells,width(inTbl)],'VariableTypes',...
+    varTypes,'VariableNames',...
     inTbl.Properties.VariableNames);
-% Change the treeName variable type to cell
-outTbl.treeIndex=num2cell(outTbl.treeIndex);
-for tr=1:3
+% Go over each L5A cell and combine the results from the two annotations
+for tr=1:numL5Acells
     curTreeName=sprintf([L5Atag,'%0.2u'],tr);
     indices=contains(inTbl.treeIndex,curTreeName);
     outTbl{tr,'treeIndex'}{1}=curTreeName;
-    outTbl{tr,2:end}=sum(inTbl{indices,2:end},1);
+    outTbl{tr,2:end}=fun(inTbl{indices,2:end});
 end
 end
 
