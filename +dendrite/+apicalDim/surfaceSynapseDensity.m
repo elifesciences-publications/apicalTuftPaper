2ddc42264e476be2d79ca60d0198821a926e651e
Ali Karimi <ali.karimi@brain.mpg.de>
2019-08-06 14:26:12 +0200

    Surface density plots done v1. Todo: 1. Combine the inhibitory and excitatory synapse densities in one correlation plot with double log scale 2. Get a linear regression line from all the data points and use in all figures

--- +dendrite/+apicalDim/surfaceSynapseDensity.m
+++ +dendrite/+apicalDim/surfaceSynapseDensity.m
@@ -46,7 +46,6 @@ results.l235.Properties.VariableNames={'mainBifurcation','distalAD'};
 x_width=[8.5, 10, 10];
 y_width=[10, 10, 10];
 util.setColors;
-colors={l2color;dlcolor};
 outputFolder=fullfile(util.dir.getFig3,...
     'synapseDensityPerUnitSurface');
 util.mkdir(outputFolder)
@@ -57,190 +56,107 @@ curResultStruct=...
     dendrite.apicalDim.surfaceDensity.outputForPlot(curResultsTables);
 
 % Diameter comparison
-fh=figure;ax=gca;
-util.plot.boxPlotRawOverlay(curResultStruct.diameter,1:2,'ylim',3.5,...
+fname='Small_apicalDiameter';
+fh=figure('Name',fname);ax=gca;
+util.plot.boxPlotRawOverlay(curResultStruct.diameter,1:2,...
     'boxWidth',0.5,'color',curColors);
-xticklabels([]);
-ylabel([]);
 xlim([0.5,2.5])
 util.plot.cosmeticsSave...
     (fh,ax,x_width(1),y_width(1),outputFolder,...
-    'Small_apicalDiameter.svg','off','on');
+    [fname,'.svg'],'off','on');
 pvalDiameterSmall=...
-    util.stat.ranksum(combineDiameterSmall{1},combineDiameterSmall{2});
+    util.stat.ranksum(curResultStruct.diameter{1},curResultStruct.diameter{2});
 
 % Excitatory surface vs pathlength synapse densities
-fhEx=figure;axEx=gca;
+fname='Small_Excitatory_pathandSurfaceDensity';
+fhEx=figure('Name',fname);axEx=gca;
 util.plot.correlation(curResultStruct.excDensity,curColors);
 util.plot.addLinearFit(curResultStruct.excDensity);
 util.plot.cosmeticsSave...
     (fhEx,axEx,x_width(2),y_width(2),outputFolder,...
-    'Small_Excitatory_pathandSurfaceDensity.svg','on','on');
+    [fname,'.svg'],'on','on');
 % Same for inhibitory
-fhInh=figure;axInh=gca;
+fname='Small_Inhibitory_pathandSurfaceDensity';
+fhInh=figure('Name',fname);axInh=gca;
 util.plot.correlation(curResultStruct.inhDensity,curColors);
 util.plot.addLinearFit(curResultStruct.inhDensity);
 util.plot.cosmeticsSave...
     (fhInh,axInh,x_width(3),y_width(3),outputFolder,...
-    'Small_Inhibitory_pathandSurfaceDensity.svg','on','on');
+    [fname,'.svg'],'on','on');
 
 %% Comparison of L2, L3 and L5 at the main bifurcation: PPC2 dataset
-curDiameters=cellfun(@(x) x.apicalDiameter,results.l235.PPC2,...
-    'UniformOutput',false);
-util.mkdir(outputFolder)
-x_width=12;
-y_width=10;
-colors={l2color;l3color;l5color};
-% Density plot
-fh=figure;ax=gca;
-util.plot.boxPlotRawOverlay(curDiameters(:),1:3,'ylim',3.5,...
-    'boxWidth',0.5,...
-    'color',colors(:));
-xticklabels([]);
-ylabel([]);
-xlim([0.5,3.5])
-util.plot.cosmeticsSave...
-    (fh,ax,x_width,y_width,outputFolder,'apicalDiameterPPC2.svg','off','on');
-pvalDiameterPPC2=...
-    kruskalwallis([curDiameters{1};curDiameters{2};curDiameters{3}],...
-    [repmat({'L2'},size(curDiameters{1}));...
-    repmat({'L3'},size(curDiameters{2}));...
-    repmat({'L5'},size(curDiameters{3}))]);
-disp (['pvalue in PPC2 dataset, main bifurcation annotations kruskallWallis test=',...
-    num2str(pvalDiameterPPC2)])
-
-%% Comparison of different cell types distal innervation: LPtA and PPC2 datasets
-curDiameters=cellfun(@(x) x.apicalDiameter,results.l235.LPtA,...
-    'UniformOutput',false);
-util.mkdir(outputFolder)
-util.setColors
-
-colors={l2color;l3color;l5color};
-% Density plot
-fh=figure;ax=gca;
-util.plot.boxPlotRawOverlay(curDiameters(:),1:3,'ylim',3.5,...
-    'boxWidth',0.5,...
-    'color',colors(:));
-xticklabels([]);
-ylabel([]);
-xlim([0.5 ,3.5])
-util.plot.cosmeticsSave...
-    (fh,ax,x_width,y_width,outputFolder,'apicalDiameterLPtA.svg','off','on');
-pvalDiameterLPtA=...
-    kruskalwallis([curDiameters{1};curDiameters{2};curDiameters{3}],...
-    [repmat({'L2'},size(curDiameters{1}));...
-    repmat({'L3'},size(curDiameters{2}));...
-    repmat({'L5'},size(curDiameters{3}))]);
-disp (['pvalue of small diameter kruskallWallis test=',...
-    num2str(pvalDiameterLPtA)])
-%% Plot the surface area synapse densities
-% Smaller datasets: S1, V2, PPC and ACC
-x_width=8.5;
-y_width=10;
+cellTypes2Include=1:5;
+curResultsTables=results.l235.mainBifurcation;
+curResultStruct=...
+    dendrite.apicalDim.surfaceDensity.outputForPlot(curResultsTables);
+curLabels=results.l235.Properties.RowNames(cellTypes2Include);
+% Constants
 outputFolder=fullfile(util.dir.getFig3,...
     'synapseDensityPerUnitSurface');
-inhSurfDensity=cellfun(@(x) x.inhSurfDensity,results.bifur.Variables,...
-    'UniformOutput',false);
-excSurfDensity=cellfun(@(x) x.excSurfDensity,results.bifur.Variables,...
-    'UniformOutput',false);
-combineDensitySmall={cat(1,excSurfDensity{1,:}),...
-    cat(1,excSurfDensity{2,:});cat(1,inhSurfDensity{1,:}),...
-    cat(1,inhSurfDensity{2,:})};
-util.mkdir(outputFolder)
-colors=repmat({exccolor;inhcolor},1,2);
-% Density plot
-fh=figure;ax=gca;
-xlength=4;
-util.plot.boxPlotRawOverlay(combineDensitySmall(:),1:xlength,'ylim',1,...
-    'boxWidth',0.5,...
-    'color',colors(:));
-xticklabels([]);
-ylabel([]);
-set(ax,'yscale','log');
-xlim([0.5,xlength+0.5]);
-ylim([0.004,2]);
+x_width=[8.5, 10, 10];
+y_width=[10, 10, 10];
+curColors={l2color;l3color;l5color;l2MNcolor;l5Acolor};
+% Diameter comparison
+fh=figure('Name',fname);ax=gca;
+util.plot.boxPlotRawOverlay(curResultStruct.diameter,1:length(cellTypes2Include),...
+    'boxWidth',0.5,'color',curColors);
+xlim([0.5,length(cellTypes2Include)+.5])
 util.plot.cosmeticsSave...
-    (fh,ax,x_width,y_width,outputFolder,'densitySurfSmall.svg','off','on');
-pvalexcSurfSmall=...
-    ranksum(combineDensitySmall{1,1},combineDensitySmall{1,2});
-disp (['pvalue of excitatory surface synapseDensity ranksum test=',...
-    num2str(pvalexcSurfSmall)])
+    (fh,ax,x_width(1),y_width(1),outputFolder,...
+    'PPC2_mainBifurcation_apicalDiameter.svg','off','on');
+util.stat.KW(curResultStruct.diameter,curLabels);
 
-pvalinhSurfSmall=...
-    ranksum(combineDensitySmall{2,1},combineDensitySmall{2,2});
-disp (['pvalue of inhibitory surface synapseDensity ranksum test=',...
-    num2str(pvalDiameterSmall)])
-%% PPC2 dataset densities
-x_width=12;
-y_width=10;
-inhSurfDensity=cellfun(@(x) x.inhSurfDensity,results.l235.PPC2,...
-    'UniformOutput',false);
-excSurfDensity=cellfun(@(x) x.excSurfDensity,results.l235.PPC2,...
-    'UniformOutput',false);
-util.mkdir(outputFolder)
-densities=[excSurfDensity';inhSurfDensity'];
-colors=repmat({exccolor;inhcolor},1,3);
-% Density plot
-fh=figure;ax=gca;
-xlength=6;
-util.plot.boxPlotRawOverlay(densities(:),1:xlength,'ylim',2,...
-    'boxWidth',0.5,...
-    'color',colors(:));
-xticklabels([]);
-ylabel([]);
-xlim([0.5,xlength+.5]);
-ylim([0.004,2]);
-
-set(ax,'yscale','log');
+% Excitatory surface vs pathlength synapse densities
+fhEx=figure('Name',fname);axEx=gca;
+util.plot.correlation(curResultStruct.excDensity,curColors);
+util.plot.addLinearFit(curResultStruct.excDensity);
 util.plot.cosmeticsSave...
-    (fh,ax,x_width,y_width,outputFolder,'densitySurfPPC2.svg','off','on');
-pvalExcDensityPPC2=...
-    kruskalwallis([densities{1,1},densities{1,2},densities{1,3}]);
-disp (['pvalue of excitatory synapse density ppc2 kruskallWallis test=',...
-    num2str(pvalDiameterPPC2)]);
-
-pvalInhDensityPPC2=...
-    kruskalwallis([densities{2,1},densities{2,2},densities{2,3}]);
-disp (['pvalue of inhibitory synapse density ppc2 kruskallWallis test=',...
-    num2str(pvalDiameterPPC2)])
-
-%% LPtA densities
-inhSurfDensity=cellfun(@(x) x.inhSurfDensity,results.l235.LPtA,...
-    'UniformOutput',false);
-excSurfDensity=cellfun(@(x) x.excSurfDensity,results.l235.LPtA,...
-    'UniformOutput',false);
-densities=[excSurfDensity';inhSurfDensity'];
-
-util.mkdir(outputFolder)
-util.setColors
-
-colors=repmat({exccolor;inhcolor},1,3);
-% Density plot
-fh=figure;ax=gca;
-xlength=6;
-util.plot.boxPlotRawOverlay(densities(:),1:xlength,'ylim',2,...
-    'boxWidth',0.5,...
-    'color',colors(:));
-xticklabels([]);
-ylabel([]);
-xlim([0.5,xlength+.5]);
-ylim([0.004,2]);
-
-set(ax,'yscale','log');
+    (fhEx,axEx,x_width(2),y_width(2),outputFolder,...
+    'PPC2_mainBifur_Excitatory_pathandSurfaceDensity.svg','on','on');
+% Same for inhibitory
+fhInh=figure('Name',fname);axInh=gca;
+util.plot.correlation(curResultStruct.inhDensity,curColors);
+util.plot.addLinearFit(curResultStruct.inhDensity);
 util.plot.cosmeticsSave...
-    (fh,ax,x_width,y_width,outputFolder,'densitySurfLPtA.svg','off','on');
-pvalExcDensityLPtA=...
-    kruskalwallis([densities{1,1};densities{1,2};densities{1,3}],...
-    [repmat({'L2'},size(densities{1,1}));...
-    repmat({'L3'},size(densities{1,2}));...
-    repmat({'L5'},size(densities{1,3}))]);
-disp (['pvalue of exc syn density LPtA kruskallWallis test=',...
-    num2str(pvalExcDensityLPtA)])
+    (fhInh,axInh,x_width(3),y_width(3),outputFolder,...
+    'PPC2_mainBifur_Inhibitory_pathandSurfaceDensity.svg','on','on');
 
-pvalInhDensityLPtA=...
-    kruskalwallis([densities{2,1};densities{2,2};densities{2,3}],...
-    [repmat({'L2'},size(densities{2,1}));...
-    repmat({'L3'},size(densities{2,2}));...
-    repmat({'L5'},size(densities{2,3}))]);
-disp (['pvalue of exc syn density LPtA kruskallWallis test=',...
-    num2str(pvalInhDensityLPtA)])
+%% Comparison of different cell types distal innervation: 
+% LPtA and PPC2 datasets
+cellTypes2Include=[1,2,3,5];
+curResultsTables=results.l235.distalAD;
+% Remove empty L2 group
+curResultsTables=curResultsTables(cellTypes2Include);
+curResultStruct=...
+    dendrite.apicalDim.surfaceDensity.outputForPlot(curResultsTables);
+% Constants
+outputFolder=fullfile(util.dir.getFig3,...
+    'synapseDensityPerUnitSurface');
+x_width=[8.5, 10, 10];
+y_width=[10, 10, 10];
+curColors={l2color;l3color;l5color;l5Acolor};
+curLabels=results.l235.Properties.RowNames(cellTypes2Include);
+% Diameter comparison
+fh=figure('Name',fname);ax=gca;
+util.plot.boxPlotRawOverlay(curResultStruct.diameter,1:length(cellTypes2Include),...
+    'boxWidth',0.5,'color',curColors);
+xlim([0.5,length(cellTypes2Include)+.5])
+util.plot.cosmeticsSave...
+    (fh,ax,x_width(1),y_width(1),outputFolder,...
+    'LPtAPPC2_distalAD_apicalDiameter.svg','off','on');
+util.stat.KW(curResultStruct.diameter,curLabels);
+
+% Excitatory surface vs pathlength synapse densities
+fhEx=figure('Name',fname);axEx=gca;
+util.plot.correlation(curResultStruct.excDensity,curColors);
+util.plot.addLinearFit(curResultStruct.excDensity);
+util.plot.cosmeticsSave...
+    (fhEx,axEx,x_width(2),y_width(2),outputFolder,...
+    'LPtAPPC2_distalAD_Excitatory_pathandSurfaceDensity.svg','on','on');
+% Same for inhibitory
+fhInh=figure('Name',fname);axInh=gca;
+util.plot.correlation(curResultStruct.inhDensity,curColors);
+util.plot.addLinearFit(curResultStruct.inhDensity);
+util.plot.cosmeticsSave...
+    (fhInh,axInh,x_width(3),y_width(3),outputFolder,...
+    'LPtAPPC2_distalAD_Inhibitory_pathandSurfaceDensity.svg','on','on');
