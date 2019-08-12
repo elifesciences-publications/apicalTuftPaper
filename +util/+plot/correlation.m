435ffa8414d1b443b8a854964e237a16b2b7a9c3
Ali Karimi <ali.karimi@brain.mpg.de>
2019-08-12 16:42:59 +0200

    Also calculate and display Pearson's R in the output of the correlation plot function

--- +util/+plot/correlation.m
+++ +util/+plot/correlation.m
@@ -1,16 +1,25 @@
-function [] = correlation(array,colors)
+function [Rho,pval] = correlation(array,colors,markers)
 % PLOTCORRELATION plots the correlation between two values. 
 % INPUT: 
 %       array: cell array with each cell having N x 2 structures. 
 %           first column is the X and second column is the Y values
 % Author: Ali Karimi <ali.karimi@brain.mpg.de>
+if ~exist('markers','var') || isempty(markers)
+    markers = repmat({'x'},size(array));
+end
 crossSize=36;
+totalX=[];totalY=[];
 hold on
 for d=1:length(array)
     curX=array{d}(:,1);
     curY=array{d}(:,2);
-    scatter(curX,curY,crossSize,colors{d},'.');
+    scatter(curX,curY,crossSize,colors{d},markers{d});
+    totalX=[totalX;curX];
+    totalY=[totalY;curY];
 end
-
+% Get the correlation coefficient and its significance level
+[Rho,pval]=corr(totalX,totalY);
+fprintf(['Correlation Coefficient is: ',num2str(Rho),'\n',...
+    'The p-value is: ',num2str(pval),'\n']);
 end
 
