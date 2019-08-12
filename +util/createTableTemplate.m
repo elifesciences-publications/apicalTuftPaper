896a3d76a5dac436b2df0b23bf16dbe7261fa56b
Ali Karimi <ali.karimi@brain.mpg.de>
2019-08-12 13:07:55 +0200

    utility to write table templates

---
+++ +util/createTableTemplate.m
@@ -0,0 +1,13 @@
+function [emptyTemplate] = createTableTemplate(tableTemplate,variableTypes)
+%CREATETABLETEMPLATE Table template from a specific Table
+tSize=size(tableTemplate);
+if  ~exist('variableTypes','var') || isempty(variableTypes)
+    variableTypes=repmat({'cell'},[1,tSize(2)]);
+end
+
+emptyTemplate=table('Size',tSize,'VariableTypes',...
+    variableTypes,'VariableNames',...
+    tableTemplate.Properties.VariableNames,'RowNames', ...
+    tableTemplate.Properties.RowNames);
+end
+
