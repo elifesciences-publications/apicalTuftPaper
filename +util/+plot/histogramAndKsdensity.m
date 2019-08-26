655af2cf01e7eae28fd87e3edbdbe5692984599d
Ali Karimi <ali.karimi@brain.mpg.de>
2019-08-26 21:19:41 +0200

    updates in kernel density/histogram plotting functions

--- +util/+plot/histogramAndKsdensity.m
+++ +util/+plot/histogramAndKsdensity.m
@@ -1,9 +1,19 @@
-function histogramAndKsdensity(data,color)
+function histogramAndKsdensity(data,color,ylimitsKS)
+% Function used for plotting the histogram and KernelDensity for figure 1H
+
+if ~exist('ylimitsKS','var') || isempty(ylimitsKS)
+    ylimitsKS=[0,15];
+end
 hold on
-yyaxis left
+ax=gca;
+yyaxis(ax,'left');
+ax.YColor=[0,0,0];
 histogram(data,...
     0:0.1:1,'FaceColor',[1,1,1],'FaceAlpha',0,'EdgeColor',color);
-yyaxis right
-util.plot.ksdensity(data,color,0.0573,[0-1e-5 1+1e-5]);
+yyaxis(ax,'right')
+ax.YColor=[0,0,0,0.5];
+% Add transparency to color
+util.plot.ksdensity(data,color(:)',0.0573,[0 1]);
+ylim(ylimitsKS)
 hold off
 end
--- +util/+plot/ksdensity.m
+++ +util/+plot/ksdensity.m
@@ -5,10 +5,12 @@ if ~exist('bounds','var') || isempty(bounds)
 end
 
 range=diff(bounds);
+% The data should be contained within the Support range so we have to add
+% 1e-5 to the bounds to make sure they contain the data
 [f,xi]=ksdensity(data,bounds(1):range/100:bounds(2),'Support',...
     [bounds(1)-1e-5 bounds(2)+1e-5],'BoundaryCorrection','reflection',...
     'Bandwidth',bandwidth);
-plot(xi,f,'Color', color);
+plot(xi,f,'-','Color', color);
 % Make sure area under the probability density estimate is =1
 assert(1-trapz(xi,f)<1e-3)
 end
