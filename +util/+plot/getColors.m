ed6c08c65b764a933eebd219c6504782b3e9dfd1
Ali Karimi <ali.karimi@brain.mpg.de>
2019-09-06 18:00:55 +0200

    correct Exc, Inh synapse collors

--- +util/+plot/getColors.m
+++ +util/+plot/getColors.m
@@ -6,8 +6,8 @@ colors.dlcolor=[243/255,146/255,0/255];
 colors.l3color=[109,181,70]./255;
 colors.l5color=[158,32,103]./255;
 colors.l5Acolor=[0,113,188]./255;
-colors.exccolor=[1,0,0];
-colors.inhcolor=[0,0,1];
+colors.exccolor=[195,0,23]./255;
+colors.inhcolor=[46,49,144]./255;
 colors.small={colors.l2color,colors.dlcolor};
 colors.l2vsl3vsl5={colors.l2color;colors.l3color;colors.l5color;...
     colors.l2MNcolor;colors.l5Acolor};
--- +util/setColors.m
+++ +util/setColors.m
@@ -10,7 +10,7 @@ assignin('base','dlcolor',[243/255,146/255,0/255]);
 assignin('base','l3color',[109,181,70]./255);
 assignin('base','l5color',[158,32,103]./255);
 assignin('base','l5Acolor',[0,113,188]./255);
-assignin('base','exccolor',[1,0,0]);
-assignin('base','inhcolor',[0,0,1]);
+assignin('base','exccolor',[195,0,23]./255);
+assignin('base','inhcolor',[46,49,144]./255);
 end
 
