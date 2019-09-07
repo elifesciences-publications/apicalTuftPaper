5eba26cc4104adc4eedffc69649374d3131a5743
Ali Karimi <ali.karimi@brain.mpg.de>
2019-09-07 14:58:10 +0200

    Script done for comparing L5tt and L5st neurons diameter and spine density

--- +dendrite/+L5A/synapticFeatures.m
+++ +dendrite/+L5A/synapticFeatures.m
@@ -1,18 +1,53 @@
 % Author: Ali Karimi <ali.karimi@brain.mpg.de>
 % Script to generate boxplots showing differences in the features  between
-% L5A and L5B neurons (diameter and spine density) 
+% L5A and L5B neurons (diameter and spine density)
 util.clearAll
 returnTable=true;
 skel.dense=apicalTuft.getObjects('l2vsl3vsl5',[],returnTable);
 skel.dim=apicalTuft.getObjects('l2vsl3vsl5Diameter',[],returnTable);
 
-synCount=apicalTuft.applyMethod2ObjectArray...
-    (skel.dense,'getSynCount',[], false, ...
+synDensity = apicalTuft.applyMethod2ObjectArray...
+    (skel.dense,'getSynDensityPerType',[], false, ...
     'mapping');
-dim=apicalTuft.applyMethod2ObjectArray...
+dim = apicalTuft.applyMethod2ObjectArray...
     (skel.dim,'getApicalDiameter',[], false, ...
     'mapping');
-diameters=cell(1,2)
-synapseDensities=cell(1,2)
+% Combine L5A and LPtA data
+[synDensity] = dendrite.l2vsl3vsl5.combineL5AwithLPtATable(synDensity);
+[dim] = dendrite.l2vsl3vsl5.combineL5AwithLPtATable(dim);
+synDensity = synDensity{[3,5],:};
+dim = dim{[3,5],:};
+
+%% Get variables ready for plotting
+forPlot.diameter = ...
+    cellfun(@(x) x.apicalDiameter,dim,'UniformOutput',false);
+forPlot.diameter = ...
+    cellfun(@(x) cellfun(@mean,x),forPlot.diameter ,'UniformOutput',false);
+% Spine density is equal to exc syn density without correction
+forPlot.spineDensity = cellfun(@(x) x.Spine,synDensity,'UniformOutput',false);
+
+%% Plot Diameter/spineDensity
+x_width=2.6;
+y_width=1.9;
+boxWidths=0.708;
+outputFolder=fullfile(util.dir.getFig3,'L5L5AComparison');
+curColors=repmat({l5color,l5Acolor},1,2);
+varNames={'diameter','spineDensity'};
+region={'mainBifurcation','distalAD'};
 for i=1:2
+    fname=['L5L5AComparison_',varNames{i}];
+    fh=figure('Name',fname);ax=gca;
+    curVariable=forPlot.(varNames{i});
+    util.plot.boxPlotRawOverlay(curVariable(:),1:4,...
+        'boxWidth',boxWidths,'color',curColors(:),'tickSize',10);
+    xlim([0.5,4.5]);
+    util.plot.cosmeticsSave...
+        (fh,ax,x_width,y_width,outputFolder,...
+        [fname,'.svg'],'off','on');
+    for j=1:2
+        disp(['Variable',varNames{i}])
+        disp(['Region:',region{j}])
+        util.stat.ranksum(curVariable{1,j},curVariable{2,j});
+    end
 end
+
