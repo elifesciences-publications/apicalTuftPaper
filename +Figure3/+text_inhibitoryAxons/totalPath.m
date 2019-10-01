e2dee4ceb5727da9d7a846cec01c425b5b9a6b55
Ali Karimi <ali.karimi@brain.mpg.de>
2019-10-01 14:43:25 +0200

    Adapt total pathlength script for axons. Write to excel file

--- +Figure3/+text_inhibitoryAxons/totalPath.m
+++ +Figure3/+text_inhibitoryAxons/totalPath.m
@@ -1,7 +1,14 @@
 %% total pathdensity of axons: used in results text
 % Author: Ali Karimi<ali.karimi@brain.mpg.de>
 apTuft=apicalTuft.getObjects('inhibitoryAxon');
+outputDir=fullfile(util.dir.getFig3,'forText_inhibitoryAxon');
+
 pathLength=apicalTuft.applyMethod2ObjectArray...
     (apTuft,'getBackBonePathLength');
-totalPatLength=rowfun(@(x) round(sum(x{1})./1000,1),pathLength(:,5));
-disp(totalPatLength)
+totalPathLength =...
+    rowfun(@(x) round(sum(x{1}.pathLengthInMicron,'all')./1000,2),...
+    pathLength(:,5));
+
+totalPathLength.Properties.VariableNames={'TotalPathLength'};
+writetable(totalPathLength,fullfile(outputDir,'TotalPathLength.xlsx'));
+util.copyfiles2fileServer;
