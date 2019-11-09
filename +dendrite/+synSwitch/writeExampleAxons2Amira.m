a63f2004416ea0cb53675041f319895f645e0d25
Ali Karimi <ali.karimi@brain.mpg.de>
2019-11-09 11:50:05 +0100

    Minor changes in plots

--- +dendrite/+synSwitch/writeExampleAxons2Amira.m
+++ +dendrite/+synSwitch/writeExampleAxons2Amira.m
@@ -1,4 +1,5 @@
 %% The shaft seeded tree
+util.clearAll
 outputFolder=fullfile(util.dir.getFig1,'AmiraForSpineTargetingRatio');
 util.mkdir(outputFolder);
 
@@ -10,28 +11,24 @@ fnames={'ShaftSeeded','SpineSeeded'};
 
 colors={inhcolor,exccolor};
 for i=1:2
-    curName=fullfile(outputFolder,fnames{i});
-    skel=apicalTuft(inputSkel{i}{:});
-    trID=skel.getTreeWithName...
+    curName = fullfile(outputFolder,fnames{i});
+    skel = apicalTuft(inputSkel{i}{:});
+    trID = skel.getTreeWithName...
         (treeNames{i});
-    skel=skel.deleteTrees(trID,true);
-    synCoords{i}=skel.getSynCoord;
-    skelTrimmed=skel.getBackBone;
+    skel = skel.deleteTrees(trID,true);
+    synCoords{i} = skel.getSynCoord;
+    skelTrimmed = skel.getBackBone;
     if ~iscell(synCoords{i}{1,2})
-        thisCoord=synCoords{i}{1,2};
-        synCoords{i}=[thisCoord;cat(1,synCoords{i}{1,3:end}{:})];
+        thisCoord = synCoords{i}{1,2};
+        synCoords{i} = [thisCoord;cat(1,synCoords{i}{1,3:end}{:})];
     else
-        synCoords{i}=cat(1,synCoords{i}{1,2:end}{:});
+        synCoords{i} = cat(1,synCoords{i}{1,2:end}{:});
     end
+    % Add the seed synapse as well
+    synCoords{i} = [synCoords{i};skel.getNodes([],skel.getSeedIdx)];
     util.amira.convertKnossosNmlToHocAll...
         (skelTrimmed,curName);
-    synCoordsNM=synCoords{i}.*skel.scale;
+    synCoordsNM = synCoords{i}.*skel.scale;
     util.amira.writeSphere(synCoordsNM,1000,[curName,'.ply'],colors{i});
 end
 system('./synPlots.sht')
-%% The spine seeded tree
-skel=apicalTuft();
-trID=skel.getTreeWithName...
-    ('layer2ApicalDendriteSeeded25');
-skel=skel.deleteTrees(trID,true);
-skel=skel.getBackBone;
