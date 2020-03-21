37b4c4052ae92cfcbea4758a68986f385a5c9297
Ali Karimi <karimi.k.ali@gmail.com>
2020-03-21 17:20:54 +0100

    Fig.3D: Note added

--- code/+Figure3/D_postsynapticSpecificity.m
+++ code/+Figure3/D_postsynapticSpecificity.m
@@ -1,30 +1,35 @@
-% setup
+%% Figure 3d: 
+% postsynaptic innervation fraction map for axons seeded from L2 and DL 
+% apical dendrites.
+
 % Author: Ali Karimi <ali.karimi@brain.mpg.de>
 
+%% setup
 util.clearAll;
-util.setColors;
-outputDir = fullfile(util.dir.getFig(3),'E_allpostsynDistribution');
+outputDir = fullfile(util.dir.getFig(3),'D');
 util.mkdir(outputDir)
-colorsDE = {[0.2 0.2 0.2],[227/255 65/255 50/255],...
-    [50/255 205/255 50/255],[50/255 50/255 205/255]}';
-x_width = 10;
-y_width = 7;
+
 apTuft= apicalTuft.getObjects('inhibitoryAxon');
 synRatio = apicalTuft.applyMethod2ObjectArray(apTuft,'getSynRatio');
 synRatio = synRatio.Variables;
 
-
-%% E 
+%% Plot Fig. 3d
 fh = figure;ax = gca;
-%Plotting
+x_width = 10;
+y_width = 7;
+% Plotting
+c = util.plot.getColors();
 hold on
 util.plot.errorbarSpecificity(synRatio(:,5),[],...
-    [{l2color},{dlcolor}]);
+    [{c.l2color},{c.dlcolor}]);
 % Figure properties
-util.plot.cosmeticsSave(fh,ax,x_width,y_width,outputDir,'E.svg');
-%% statistical testing
+util.plot.cosmeticsSave(fh,ax,x_width,y_width,outputDir,'Fig3D.svg');
+
+%% Bootstrap test to compare L2 and DL innervation fraction
+
 TestResults = util.stat.bootStrapTest(synRatio{1,5},synRatio{2,5},false);
 writetable(TestResults,fullfile(outputDir,...
-    util.addDateToFileName('E_bootStrapTestResults.txt')));
-
-
+    'Fig3D_bootStrapTestResults.txt'));
+% Note: excel data is the same as Fig. 3B 
+% The data used in this figure matches the data used in Fig. 3B. Here
+% we have all the targets and Fig.3B focuses on the AD re-innervation.
