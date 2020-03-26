4d94f877ae67074ad41691d266a8817716b9c583
Ali Karimi <karimi.k.ali@gmail.com>
2020-03-26 20:11:21 +0100

    Remove setColors

--- startup.m
+++ startup.m
@@ -3,9 +3,6 @@
 % Note: run inside the main directory (ending in apicaltuftpaper)
 addpath(genpath('code'))
 
-% Set the color variables used frequently for creating the figures
-util.setColors()
-
 % Get the file server directory (GUI prompt) and save to a mat file if the
 % matfile does not exist
 if ~exist(fullfile(util.dir.getMatfile,'remoteDir.mat'),'file')
