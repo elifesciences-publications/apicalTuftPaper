57642efd3343ccd8d72c2f1c113cc81e222aa647
Ali Karimi <karimi.k.ali@gmail.com>
2020-03-28 20:37:51 +0100

    Cleanup and documentation plus more testing

--- code/+axon/+multipleTargeting/checkTotalSynapseNumber.m
+++ code/+axon/+multipleTargeting/checkTotalSynapseNumber.m
@@ -1,19 +1,29 @@
 function [] = checkTotalSynapseNumber(skel,MultiPerDataset,results)
-% check that the total number of synapse in the apical dendrite groups
-% match with what we get from syncount
+% checkTotalSynapseNumber: check that the total number of synapse in the
+% apical dendrite group is the same before and after extraction of multiple
+% innervation
+
+% Author: Ali Karimi <ali.karimi@brain.mpg.de>
+
 seedTag = {'l2Idx','dlIdx'};
 for d = 1:length(skel)
     for seedT = 1:2
         trIndices = skel{d}.(seedTag{seedT});
-        synCount = skel{d}.getSynCount(trIndices);
-        curSynCountSum = sum(synCount{:,2:3});
+        % synCount has all synapses except the first seed synapse
+        curCount = skel{d}.getSynCount(trIndices);
+        curCountSum = sum(curCount{:,2:3},1);
         seedCounts = MultiPerDataset{d}.seedTargetingNr(trIndices,:);
+        % Take out the first seed synapse (also excluded in getSynCount method)
         seedCounts(:,seedT) = seedCounts(:,seedT)-1;
-        additionalSeedSynapses = sum(seedCounts,1);
-        allOtherSyn = sum(results{d}(:,:,seedT).*[1:10]',[1,3]);
-        disp(seedTag{seedT});
-        disp(curSynCountSum);
-        disp((allOtherSyn+additionalSeedSynapses))
+        % Sum over trees of the same seed type
+        additionalSeedCount = sum(seedCounts,1);
+        % Get the count of all non-seed synapses. Note that to get the
+        % total number of synapses, the number of targets should be multiplied by the
+        % number of synapses on each target.
+        allNonSeedSyn = sum(results{d}(:,:,seedT).*[1:10]',[1,3]);
+        % count check between the two counts
+        assert(all(curCountSum == ...
+        (allNonSeedSyn + additionalSeedCount)), 'Count per AD type Check failed')
     end
 end
 end
--- code/+axon/+multipleTargeting/extractMultipleTargeting.m
+++ code/+axon/+multipleTargeting/extractMultipleTargeting.m
@@ -1,86 +1,132 @@
-function [targetMultiplicity] = extractMultipleTargeting(skel)
-%EXTRACTMULTIPLETARGETING extracts multiple targeting from the axons with a
+function [multiTable] = extractMultipleTargeting(skel)
+% EXTRACTMULTIPLETARGETING extracts multiple targeting from the axons with a
 % naming structure which associates each synapse with its targeting index
+% INPUT: 
+%       skel: The apicalTuft object
+% OUTPUT:
+%       targetMultiplicity: table
+%           contains raw and processed multi innervation of apical
+%           dendrites. The seed multi-innervation is also reported here.
+
+% Author: Ali Karimi <ali.karimi@brain.mpg.de>
+
+% Maximum number of synapses on a postsynaptic target
 maxMultiplicity = 10;
-synIdx = skel.getSynIdx;synIdx.treeIndex = skel.treeUniqueString;
+% Get synapse Idx and number of times the seed type is targeted
+synIdx = skel.getSynIdx;
+synIdx.treeIndex = skel.treeUniqueString;
+synCount = skel.getSynCount;
 seedTargeting = skel.getSeedTargetingNumber;
-% This table saves the target order of the synaps
-% (1: first synapse on target,2: second synapse on target, so on)
-apicalLabels = {'L2Apical','DeepApical','L2ApicalRaw','DeepApicalRaw'};
-targetMultiplicity = skel.createEmptyTable([],...
-    [synIdx.Properties.VariableNames(1),apicalLabels],'cell');
-targetMultiplicity = [targetMultiplicity,seedTargeting(:,'seedTargetingNr')];
+
 % make seed targeting related to the seeding type
-seedTargetingNr = zeros(skel.numTrees,2);
+seedCount = zeros(skel.numTrees,2);
 IdxSeedType = ismember(1:skel.numTrees,skel.dlIdx)+1;
-seedTargetingNr(sub2ind(size(seedTargetingNr),...
-    1:skel.numTrees,IdxSeedType)) = ...
+seedCount(sub2ind(size(seedCount),...
+    1:skel.numTrees, IdxSeedType)) = ...
     seedTargeting{:,'seedTargetingNr'};
-targetMultiplicity.seedTargetingNr = seedTargetingNr;
+assert(all(seedTargeting{:,'seedTargetingNr'} == sum(seedCount,2)),...
+    'Check that order of the trees are not substituted')
+
+% Initialize the table for multiInnervation results and set the seed
+% targeting
+apicalLabels = {'L2Apical','DeepApical','L2ApicalRaw','DeepApicalRaw'};
+multiTable = skel.createEmptyTable([],...
+    [synIdx.Properties.VariableNames(1),apicalLabels],'cell');
+multiTable.seedTargetingNr = seedCount;
+
+% Loop over individual trees
 for tr = 1:height(synIdx)
-    % Init
-    curIdx = synIdx{tr,apicalLabels(1:2)};
+    % Initialize
+    curIdx = synIdx{tr,{'L2Apical','DeepApical'}};
     synTargetNumber = cell(1,2);
-    curTargetCount = cell(1,2);
-    for ap = 1:2
-        % More initialization
-        synTargetNumber{ap} = zeros(1,maxMultiplicity);
-        curTargetCount{ap} = zeros(1,maxMultiplicity);
-        if ~isempty(curIdx{ap})
-            curComment = {skel.nodesAsStruct{tr}(curIdx{ap}).comment};
+    curCount_noSeed = cell(1,2);
+    for targetIdx = 1:2
+        % Initialize to the maximum estimated multi-innervation
+        synTargetNumber{targetIdx} = zeros(1,maxMultiplicity);
+        curCount_noSeed{targetIdx} = zeros(1,maxMultiplicity);
+        % Only continue if synapses exist in the target group, otherwise
+        % leave as empty
+        if ~isempty(curIdx{targetIdx})
+            % Note: The number in the string represents the Id of the
+            % number on the target. _1:first synapse, _2: second synapse,
+            % _3 third synapse, so on. Seed synapses are separately noted
+            % only synapse _2, _3 on the seed is annotated. The reason for
+            % this complex approach was historical development.
+            
+            % Extract the Multi innervation annotations from the comment
+            % strings. 
+            curComments = ...
+                {skel.nodesAsStruct{tr}(curIdx{targetIdx}).comment};
             splitfun = @(curComment) ...
                 regexp(curComment,'^(\w*)_(\d)$','tokens');
-            curParts = cellfun(splitfun,curComment);
-            cellfun(@(curPart) assert(length(curPart) == 2),curParts)
-            numfun = @(curPart) uint8(str2double(curPart{2}));
-            synTargetNumber{ap} = cellfun(numfun,curParts)';
-            % Find the number of each targeting
-            curRawCount = accumarray(synTargetNumber{ap},1);
-            % Take out seed targeting in case of looking at the seed
-            % target type innervation
-            % Note seed targeting does not include the first synapse
-            % since it is called seed X
-            if seedTargetingNr(tr,ap) >0
-                curRawCount(1,1) = curRawCount(1,1)+1;
-                seedTargetingCount = accumarray([1:seedTargetingNr(tr,ap)]',...
-                    1,size(curRawCount));
-                curRawCountMinusSeed = ...
-                    curRawCount-seedTargetingCount;
-                assert(isequal(sort(curRawCountMinusSeed,'descend'),...
-                    curRawCountMinusSeed));
+            curParts = cellfun(splitfun,curComments);
+            cellfun(@(curPart) assert(length(curPart) == 2,'Length check'),...
+                curParts)
+            numConvertfun = @(curPart) uint8(str2double(curPart{2}));
+            synTargetNumber{targetIdx} = cellfun(numConvertfun,curParts)';
+            % Use accumarray to create a counting array with the first
+            % element being the number of the first synapses (_1) and so on
+            curStringCount = accumarray(synTargetNumber{targetIdx},1);
+            
+            % 0 on seed targeting means that the axon is seeded from the
+            % other AD type
+            if seedCount(tr,targetIdx) > 0
+                % Take out seed targeting and its multi innervation using
+                % information in the seed comment
+                curStringCount(1,1) = curStringCount(1,1)+1;
+                seedTargetingCount = accumarray([1:seedCount(tr,targetIdx)]',...
+                    1,size(curStringCount));
+                curMinusSeed = ...
+                    curStringCount - seedTargetingCount;
+                assert(isequal(sort(curMinusSeed,'descend'),...
+                    curMinusSeed), 'Non-ascending check');
             else
-                curRawCountMinusSeed = curRawCount;
+                curMinusSeed = curStringCount;
             end
+            
             % Now count how many of different multiple innervation
             % exist from the annotation
-            curTargetCount{ap} = zeros(1,maxMultiplicity);
+            curCount_noSeed{targetIdx} = zeros(1,maxMultiplicity);
             % maxMultiplicity is used to create arrays of the same size for
             % adding later. Make sure this arbitarary number is not smaller
             % than the largest multiplicity
-            assert(maxMultiplicity>length(curRawCountMinusSeed));
-            for curMult = length(curRawCountMinusSeed):-1:1
-                curTargetCount{ap}(curMult) = curRawCountMinusSeed(curMult);
-                curRawCountMinusSeed = curRawCountMinusSeed-...
-                    curRawCountMinusSeed(curMult);
+            assert(maxMultiplicity > length(curMinusSeed), 'Size check');
+            % Start from the highest multi string
+            for curMult = length(curMinusSeed):-1:1
+                curCount_noSeed{targetIdx}(curMult) = curMinusSeed(curMult);
+                curMinusSeed = curMinusSeed-...
+                    curMinusSeed(curMult);
             end
-            assert(all(curTargetCount{ap} >= 0));
-            % Total synapses = seed synapses + non-seed synapses
-            if seedTargetingNr(tr,ap) >0
-                assert(sum(curTargetCount{ap}.*(1:maxMultiplicity))+...
-                    seedTargetingNr(tr,ap) == length(curComment)+1)
-            else
-                assert(sum(curTargetCount{ap}.*(1:maxMultiplicity))+...
-                    seedTargetingNr(tr,ap) == length(curComment))
-            end
+            assert(all(curCount_noSeed{targetIdx} >= 0), ...
+                'Positivity check');
+            % In case the target type matches the seeding type an
+            % additional synapse should be added (The initial seed synapse)
+            seedPresence = uint8(seedCount(tr,targetIdx) > 0);
+            totalCurCount = sum(...
+                curCount_noSeed{targetIdx}.*(1:maxMultiplicity)) + ...
+                seedCount(tr,targetIdx);
+            assert(totalCurCount == length(curComments)+seedPresence, ...
+                'total number check');
+            % Another check: use data from Get syn Count
+            % Note: synCount gives the number of synapses minus the first
+            % seed synapse, for results saved here we have taken out all
+            % seed synapses. Therefore curCount_noSeed has X synapses less
+            % than the total synapses. X is the number of all synapses on
+            % the seed Apical dendrite
+            assert(totalCurCount == ...
+                synCount{tr, targetIdx+1}+seedPresence,...
+                'total number check getSynCount')
         end
     end
-    targetMultiplicity{tr,{'L2Apical','DeepApical'}}...
-        =curTargetCount;
-    targetMultiplicity{tr,{'L2ApicalRaw','DeepApicalRaw'}}...
-        =synTargetNumber;
+    
+    % Assignments
+    multiTable{tr,{'L2Apical','DeepApical'}}...
+        = curCount_noSeed;
+    multiTable{tr,{'L2ApicalRaw','DeepApicalRaw'}}...
+        = synTargetNumber;
 end
-% switch to double
-targetMultiplicity.L2Apical = cell2mat(targetMultiplicity.L2Apical);
-targetMultiplicity.DeepApical = cell2mat(targetMultiplicity.DeepApical);
+% Convert to double
+multiTable.L2Apical = cell2mat(multiTable.L2Apical);
+multiTable.DeepApical = cell2mat(multiTable.DeepApical);
 end
 
